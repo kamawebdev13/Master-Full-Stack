4956c962811b4deb03fdc154c071cde79d5828a8
kamawebdev13 <karinadatanalyst13@alumnos.cei.es>
2026-02-24 21:28:11 +0100

    feat: add initial TypeScript file with user profile examples

---
+++ egle/TypeScript/retoUno.js
@@ -0,0 +1,14 @@
+var usuario1 = {
+    userName: "Pepeto Lopez",
+    email: "pepeto13@yahoo.com",
+    edad: 55,
+    biografia: "Galan de barrio",
+    telefono: 5678945
+};
+var usuario2 = {
+    userName: "Maruja Majo",
+    email: "lamaruja73@yahoo.com",
+    edad: 58,
+};
+console.log(usuario1);
+console.log(usuario2);
